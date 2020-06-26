635b52aa3ae32a31448e026ce71294ec8ffe6883
zhangjian <985063447@qq.com>
2020-06-26 21:28:37 +0800

    06-26

---
+++ day01-code/src/map/HashMapDemo.java
@@ -0,0 +1,57 @@
+package map;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+// 使用链表存储数据
+public class HashMapDemo {
+    Node head = null; // 队列的头部
+
+    public void add(Node node) {
+        if (head == null) {
+            head = node;
+            return;
+        }
+        // 追加到队列尾部
+        Node temp = head;
+        while (temp.next != null) { // 找到一个next为空的节点，这个节点就是最后一个
+            temp = temp.next;
+        }
+        temp.next = node;
+    }
+
+    public void print() { // 打印链表内容
+        Node temp = head;
+        while (temp.next != null) {
+            System.out.println(temp.key);
+            temp = temp.next;
+        }
+        System.out.println(temp.key);
+    }
+
+    public static void main(String[] args) throws IOException {
+        HashMap<Integer,Integer> map = new HashMap();
+        HashMapDemo hashMapDemo = new HashMapDemo();
+        Node node1 = new Node();
+        node1.key = "1";
+        hashMapDemo.add(node1);
+
+        Node node2 = new Node();
+        node2.key = "4";
+        hashMapDemo.add(node2);
+
+        Node node3 = new Node();
+        node3.key = "3";
+        hashMapDemo.add(node3);
+
+        hashMapDemo.print();
+
+    }
+}
+
+class Node {
+    public String key;
+    public String value;
+
+    public Node next;
+}
